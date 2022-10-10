33dd154152e7ae187098dc5546614cc804f54e22
Vitalii Travin <vitalii.trvn@gmail.com>
2022-10-10 14:31:20 +0600

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -19,18 +19,10 @@ module.exports = {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: "asset/resource",
       },
-      // {
-      //   test: /\.(gif|png|jpe?g|svg)$/i,
-      //   use: "file-loader",
-      // },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
         type: "asset/inline",
       },
-      // {
-      //   test: /\.(png|jpg)$/,
-      //   loader: "url-loader",
-      // },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
